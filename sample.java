7439eb5d648fb58b7656a8622d770d3c63393b59
TarunJaggarapu <tarun18teja@gmail.com>
2021-08-15 13:36:27 +0530

    deleted a line

--- sample.java
+++ sample.java
@@ -2,6 +2,5 @@ public class sample {
     public static void main(String[] args) {
         System.out.println("GitHub");
         System.out.println("new line added");
-        System.out.println("new line added2");
     }
 }
