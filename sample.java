b4546bd349ed4759374c9a4a213ebeb3bbf9ab10
TarunJaggarapu <tarun18teja@gmail.com>
2021-08-15 13:36:52 +0530

    Added a comment

--- sample.java
+++ sample.java
@@ -1,6 +1,7 @@
 public class sample {
     public static void main(String[] args) {
         System.out.println("GitHub");
+        // Add comment
         System.out.println("new line added");
     }
 }
