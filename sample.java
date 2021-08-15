982b8fc4f845d2c01fca581eecd2ac5160e8f38a
TarunJaggarapu <88957931+TarunJaggarapu@users.noreply.github.com>
2021-08-15 13:47:19 +0530

    Delete sample.java

--- sample.java
+++
@@ -1,7 +0,0 @@
-public class sample {
-    public static void main(String[] args) {
-        System.out.println("GitHub");
-        // Add comment
-        System.out.println("new line added");
-    }
-}
